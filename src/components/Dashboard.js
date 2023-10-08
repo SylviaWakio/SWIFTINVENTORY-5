0bc97d8d27cb3e9869a1130bb110329ef1322e32
Farah <sumeyafarah1714@gmail.com>
2023-10-08 22:14:20 +0300

    Removed Folder

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserList from './UserList';
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* Render the UserList component within the Dashboard */}
+      <UserList />
+      <div className="text-center ">
+        {/* Add a Link to navigate to the Users.js file as a button */}
+       
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
